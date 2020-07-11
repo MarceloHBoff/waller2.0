5947a0e1b1b7872b46fe4615eca9de5fdfb2d4bb
MarceloHBoff <marceloboff20@gmail.com>
2020-07-11 01:06:02 -0300

    Init dashboard page

--- waller/src/pages/Dashboard/index.tsx
+++ waller/src/pages/Dashboard/index.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, StatusBar } from 'react-native';
 
 import { useAuth } from '../../hooks/auth';
+import { Colors } from '../../styles';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderText,
+  Cards,
+  Card,
+  CardTitle,
+  CardValue,
+} from './styles';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  const greeting = () => {
+    const nowHour = new Date().getHours();
+
+    if (nowHour > 5) return 'Good morning';
+    if (nowHour > 12) return 'Good afternoon';
+    return 'Good night';
+  };
 
   return (
     <Container>
+      <StatusBar backgroundColor={Colors.primarySuperDark} />
+      <Header style={{ elevation: 10 }}>
+        <HeaderText>
+          {greeting()}, {'\n'}
+          {user.name}
+        </HeaderText>
+      </Header>
+      <Cards horizontal showsHorizontalScrollIndicator={false}>
+        <Card>
+          <CardTitle>Equity</CardTitle>
+          <CardValue>R$ 1000,00</CardValue>
+        </Card>
+        <Card>
+          <CardTitle>Equity</CardTitle>
+          <CardValue>R$ 1000,00</CardValue>
+        </Card>
+        <Card>
+          <CardTitle>Equity</CardTitle>
+          <CardValue>R$ 1000,00</CardValue>
+        </Card>
+      </Cards>
       <TouchableOpacity onPress={signOut}>
         <Text>Siar</Text>
       </TouchableOpacity>
--- waller/src/pages/Dashboard/styles.ts
+++ waller/src/pages/Dashboard/styles.ts
@@ -1,3 +1,51 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import { Colors, Fonts, Metrics } from '../../styles';
+
+export const Container = styled.View`
+  flex: 1;
+  background: ${Colors.grayDarker};
+`;
+
+export const Header = styled.View`
+  height: 80px;
+  border-bottom-left-radius: ${Metrics.radius * 2}px;
+  border-bottom-right-radius: ${Metrics.radius * 2}px;
+  background: ${Colors.primarySuperDark};
+`;
+
+export const HeaderText = styled.Text`
+  color: ${Colors.white};
+  font-size: ${Fonts.regular}px;
+  font-family: ${Fonts.poppinsMedium};
+  padding: ${Metrics.base}px;
+`;
+
+export const Cards = styled.ScrollView`
+  max-height: 220px;
+`;
+
+export const Card = styled.View`
+  border-radius: ${Metrics.radius}px;
+  background: ${Colors.gray};
+  margin: ${Metrics.base}px;
+  padding: ${Metrics.base}px;
+
+  width: 200px;
+  height: 200px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CardTitle = styled.Text`
+  color: ${Colors.white};
+  font-size: ${Fonts.regular}px;
+  font-family: ${Fonts.poppinsMedium};
+`;
+
+export const CardValue = styled.Text`
+  color: ${Colors.white};
+  font-size: ${Fonts.regular}px;
+  font-family: ${Fonts.poppinsMedium};
+`;
